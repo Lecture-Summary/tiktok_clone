cd744b7013f95ff713db25063bff37cb3a8d7c54
HoJin9622 <kiss0104040@gmail.com>
2023-06-04 13:16:27 +0900

    #18.4 CustomTransitionPage

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -9,7 +9,8 @@ import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
 import 'package:tiktok_clone/generated/l10n.dart';
 
 class SignUpScreen extends StatelessWidget {
-  static String routeName = '/';
+  static const routeURL = '/';
+  static const routeName = 'signUp';
   const SignUpScreen({super.key});
 
   void _onLoginTap(BuildContext context) async {
@@ -39,7 +40,7 @@ class SignUpScreen extends StatelessWidget {
     //         );
     //       }),
     // );
-    context.push(UsernameScreen.routeName);
+    context.pushNamed(UsernameScreen.routeName);
   }
 
   void _onAppleTap(BuildContext context) {}
